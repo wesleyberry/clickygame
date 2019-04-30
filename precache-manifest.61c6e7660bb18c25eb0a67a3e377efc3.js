9a2242b4dca07e9cbc637ebc813361c9d22d0d48
Wesley Berry <wesleyberry52@gmail.com>
2019-04-30 18:38:09 -0500

    Updates

---
+++ precache-manifest.61c6e7660bb18c25eb0a67a3e377efc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1018ab77ae9563e7a80fcb2724ae32f5",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f6e881fc68f159679b3d",
+    "url": "/clickygame/static/css/main.8633577c.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f6e881fc68f159679b3d",
+    "url": "/clickygame/static/js/main.2c6c283f.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
